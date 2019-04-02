40da7434d4e80d127aa3219aef448f7c48800f09
mnrotrmedic <33576579+mnrotrmedic@users.noreply.github.com>
2019-04-02 14:30:05 -0500

    Server working

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+const burger = require("../models/burger")
+
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        var hbsObject = {
+            burgers: data
+        }
+        res.render("index", hbsObject);
+    })
+})
+
+router.post("/", function (req, res) {
+    burger.create([
+        "burger_name", "devoured"
+    ],
+        [
+            req.body.burger_name, req.body.devoured
+        ], function () {
+            res.render("index")
+        })
+})
+
+module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,20 @@
-const orm = require("./config/orm.js");
+const orm = require("../config/orm");
 
 var burger = {
-    a
+    all: function (callback) { //Get all burgers from the database
+        orm.selectAll("burgers", function (result) {
+            callback(result);
+        }
+        )
+    },
+
+    create: function (cols, vals, callback) { //Add a new burger to the database
+        orm.insertOne("burgers", cols, vals, function (result) {
+            callback(result)
+        })
+    },
+
+    // update: function(objColVals, condition)
 }
+
+module.exports = burger;
