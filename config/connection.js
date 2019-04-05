7d88cba4661b0ef4a16bf8e1786e801a868f7982
mnrotrmedic <33576579+mnrotrmedic@users.noreply.github.com>
2019-04-04 20:18:29 -0500

    Heroku

--- config/connection.js
+++ config/connection.js
@@ -10,6 +10,17 @@ var connection = mysql.createConnection({
     database: "burgers_db"
 });
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "fnx6frzmhxw45qcb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        user: "akj9x5xqomjtsg4x",
+        password: "jo6xjypdw7x8taib",
+        database: "fguh24ee4kqevej4"
+    });
+};
+
 connection.connect(function (err) {
     if (err) {
         console.error("Oh no! Error Connecting: " + err.stack);
